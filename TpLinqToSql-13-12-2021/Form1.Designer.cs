7bd1b7a3f362a0b03ee0baa640e392f68edf0f2c
elhajuojy <souirimehdi311@gmail.com>
2021-12-15 22:05:29 +0100

    Linq to Sql Practice version 1

---
+++ TpLinqToSql-13-12-2021/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace TpLinqToSql_13_12_2021
+{
+    partial class LinqToSql
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LinqToSql));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnOrderSalire = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(736, 258);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(677, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // btnOrderSalire
+            // 
+            this.btnOrderSalire.Location = new System.Drawing.Point(852, 117);
+            this.btnOrderSalire.Name = "btnOrderSalire";
+            this.btnOrderSalire.Size = new System.Drawing.Size(236, 38);
+            this.btnOrderSalire.TabIndex = 2;
+            this.btnOrderSalire.Text = "Order  Salary Asecending";
+            this.btnOrderSalire.UseVisualStyleBackColor = true;
+            this.btnOrderSalire.Click += new System.EventHandler(this.btnOrderSalire_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(852, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(236, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Order EmpName Descending";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 414);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(236, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Get Selected Columns";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(852, 205);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 38);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Count Employees by Job ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(852, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(236, 38);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Count Empolyees by Departement   >5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(852, 293);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(236, 38);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Count Emplyees by dep > 5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(383, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 38);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "LinQ to Sql";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.checkedListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBox1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedListBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.HorizontalScrollbar = true;
+            this.checkedListBox1.ImeMode = System.Windows.Forms.ImeMode.KatakanaHalf;
+            this.checkedListBox1.IntegralHeight = false;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "EmpID",
+            "EmpName",
+            "Job ",
+            "HireDate",
+            "Salary",
+            "DeptNO"});
+            this.checkedListBox1.Location = new System.Drawing.Point(62, 417);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(121, 142);
+            this.checkedListBox1.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(852, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Count Emplyees by job < 5";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(55, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 38);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Employees";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(852, 381);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(236, 38);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "select All";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // LinqToSql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1167, 571);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnOrderSalire);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LinqToSql";
+            this.Text = "LinQ to Sql ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnOrderSalire;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ TpLinqToSql-13-12-2021/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.Linq;
+
+namespace TpLinqToSql_13_12_2021
+{
+    public partial class LinqToSql : Form
+    {
+        public LinqToSql()
+        {
+            InitializeComponent();
+        }
+        CompanyDBDataContext db = new CompanyDBDataContext();
+        
+        //Table<Employee> emp;
+        e
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.AllowUserToAddRows = false;
+            //emp=db.GetTable<Employee>();
+
+
+
+            var reqSelect = (from jobs in db.Employees select jobs.Job).Distinct();
+
+          
+
+            //comboBox1.DataSource = db.Employees.ToList();
+            //comboBox1.DisplayMember = "Job";
+            //comboBox1.ValueMember = "Job";
+
+
+            //Method 1 and the best one 
+
+
+            comboBox1.DataSource=reqSelect;
+            
+
+
+
+            
+        }
+
+        private void btnOrderSalire_Click(object sender, EventArgs e)
+        {
+            var reqOrder = from emp in db.Employees
+                           orderby emp.Salary ascending
+                           select emp;
+            
+            dataGridView1.DataSource=reqOrder;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var reqOrderbyName = from emp in db.Employees
+                           orderby emp.EmpName descending
+                           select emp;
+            dataGridView1.DataSource = reqOrderbyName;
+        }
+        List<string> liststr;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str1 = string.Empty ;
+            foreach (string strcheck in checkedListBox1.CheckedItems)
+            {
+                liststr.Add(strcheck);
+            }
+            for(int i = 0; i < liststr.Count - 2; i++)
+            {
+                str1 += "emp." + liststr[i] + ",";
+            }
+            str1+="emp."+liststr[liststr.Count - 1];
+            Object obj=new Object();
+            obj = str1.Replace("\"",""); 
+
+            var reqSelectedColumns = from emp in db.Employees
+                                     select obj;
+                                    
+            dataGridView1.DataSource = reqSelectedColumns;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var reqGroupjob = from job in db.Employees
+                     group job by job.Job into grpJob
+                     select new { Job = grpJob.Key, NbrEmployess = grpJob.Count() };
+
+            dataGridView1.DataSource= reqGroupjob;
+            dataGridView1.AutoSizeColumnsMode=DataGridViewAutoSizeColumnsMode.Fill;
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            var pl = from empDep in db.Employees
+                     group empDep by empDep.DeptNO into grpdep
+                     select new { deptno = grpdep.Key, Nbrdep = grpdep.Count() };
+
+            dataGridView1.DataSource = pl;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var reqGroupjob = from job in db.Employees
+                              group job by job.DeptNO into grpJob
+                              where grpJob.Count() >=5
+                              
+                              select new { dep = grpJob.Key, NbrEmployess = grpJob.Count() };
+
+            dataGridView1.DataSource = reqGroupjob;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            
+
+            var selectbyJob = from emp in db.Employees
+                              where emp.Job == comboBox1.SelectedValue.ToString()
+                              select emp;
+
+
+
+            dataGridView1.DataSource = selectbyJob;
+        }
+         
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var reqGroupjob = from job in db.Employees
+                              group job by job.Job into grpJob
+                              where grpJob.Count() < 5
+                              select new { Job = grpJob.Key, NbrEmployess = grpJob.Count() };
+
+            dataGridView1.DataSource = reqGroupjob;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
